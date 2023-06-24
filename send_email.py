b7ce9f1194d1f9ad6b44369058053364fbadcf13
saibharghav <64954492+saibharghav@users.noreply.github.com>
2023-06-24 14:39:13 -0400

    Assign environment variables

--- send_email.py
+++ send_email.py
@@ -1,19 +1,15 @@
 import smtplib
 import ssl
+import os
 
-host = "smtp.gmail.com"
-port = 465
 
-username = "chollangi72@gmail.com"
-password = "vhcgvbxxtsbxfnah"
-receiver = "chollangi72@gmail.com"
-context = ssl.create_default_context()
-
-message = """\
-Subject: Hi! 
-How are you?
-Bye
-"""
-with smtplib.SMTP_SSL(host, port, context=context) as server:
-    server.login(username, password)
-    server.sendmail(username, receiver, message)
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+    username = "chollangi72@gmail.com"
+    password = os.getenv("PASSWORD")
+    receiver = "chollangi72@gmail.com"
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
